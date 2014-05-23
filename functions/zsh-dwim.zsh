b63403fbbc7373500d3c06fa3d0f6086f86a0ea6
Srijan R Shetty <srijan.shetty@gmail.com>
2014-05-23 11:40:51 +0530

    More dwin transformations

--- functions/zsh-dwim.zsh
+++ functions/zsh-dwim.zsh
@@ -8,6 +8,10 @@ _dwim_add_transform '^rm' \
 _dwim_add_transform '^rm -f' \
   '_dwim_sed "s/^rm -f /rm -rf /"'
 
+## rm -rf -> sudo rm -rf
+_dwim_add_transform '^rm -rf' \
+  '_dwim_sed "s/^rm -rf /sudo rm -rf /"'
+
 ## cp -> cp -R
 _dwim_add_transform '^cp ' \
   '_dwim_sed "s/^cp /cp -R /"'
