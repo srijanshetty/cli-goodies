c6f9589c8752334719fa571904e47798614f3d6e
Srijan R Shetty <srijan.shetty@gmail.com>
2014-05-29 23:24:08 +0530

    gulp completions

--- init.zsh
+++ init.zsh
@@ -1,10 +1,14 @@
 #!/bin/zsh
 
+# For all the completion scripts
+# fpath=("${0:h}/completion/" $fpath)
+
 # Source every file in custom
 for file in $(ls "${0:h}/functions"); do
     source "${0:h}/functions/$file"
 done
 
+# For any external imported files
 for file in $(ls "${0:h}/external"); do
     source "${0:h}/external/$file"
 done
