f93bf188b6da70a7a55c986544237a85c51dc7f8
Srijan R Shetty <srijan.shetty@gmail.com>
2014-09-11 17:39:53 +0530

    Tmux

--- functions/tmux.zsh
+++ functions/tmux.zsh
@@ -1,38 +1,6 @@
 # Startup configuration for tmux
 function startt() {
     case $1 in
-        "mwos")
-            # Create a new tmux-session
-            SESSION="firewall"
-            tmux -2 new-session -d -s $SESSION
-
-            # Create the windows
-            tmux rename-window -t $SESSION:1 firewall
-
-            # Start defualt programs in them
-            tmux send-keys -t $SESSION:1 "auth" C-m
-
-            # Create another tmux-sesion for working
-            SESSION="MWOS"
-            tmux -2 new-session -d -s $SESSION
-
-            # General purpose
-            tmux rename-window -t $SESSION:1 general-purpose
-            tmux split-window -h
-            tmux select-pane -t 1
-            tmux resize-pane -R 30
-
-            # The vim window
-            tmux new-window -t $SESSION -a -n vim
-
-            # An empty window
-            tmux new-window -t $SESSION -a -n irc 'irssi'
-
-            # Attach the session
-            tmux select-window -t $SESSION:3
-            tmux attach -t $SESSION
-            ;;
-
         "iitk")
             # Create a new tmux-session
             SESSION="firewall"
@@ -88,6 +56,30 @@ function startt() {
             tmux select-window -t $SESSION:3
             tmux attach -t $SESSION
             ;;
+
+        *)
+            # Create another tmux-sesion for working
+            SESSION="General"
+            tmux -2 new-session -d -s $SESSION
+
+            # General purpose
+            tmux rename-window -t $SESSION:1 general-purpose
+            tmux send-keys -t $SESSION:1 "autostart" C-m
+            tmux split-window -h
+            tmux select-pane -t 1
+            tmux resize-pane -R 30
+
+            # The vim window
+            tmux new-window -t $SESSION -a -n vim
+
+            # An empty window
+            tmux new-window -t $SESSION -a -n irc 'irssi'
+
+            # Attach the session
+            tmux select-window -t $SESSION:3
+            tmux attach -t $SESSION
+            ;;
+
     esac
 }
 
