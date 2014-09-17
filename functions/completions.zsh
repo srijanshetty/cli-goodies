e459fc097df34d7915723ad563fdf3d6395c753e
Srijan R Shetty <srijan.shetty@gmail.com>
2014-09-17 16:35:51 +0530

    Removed unwanted files

--- functions/completions.zsh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-
-# For npm completions
-# https://www.npmjs.org/doc/cli/npm-completion.html
-# . <(npm completion)
--- functions/updates.zsh
+++
@@ -1,4 +0,0 @@
-function mbox-update() {
-    wget http://pdos.csail.mit.edu/mbox/mbox-latest-amd64.deb > /dev/null && sudo dpkg -i mbox-latest-amd64.deb \
-        && rm -rf mbox-latest-amd64.deb
-}
