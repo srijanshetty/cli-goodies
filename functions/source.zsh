1425d6805bd32ba2a72d0bc63da06d55f77ed8e8
Srijan R Shetty <srijan.shetty@gmail.com>
2014-09-24 15:04:05 +0530

    Moved node config to Prezto mai

--- functions/source.zsh
+++ functions/source.zsh
@@ -9,10 +9,6 @@
 # Colors :D
 eval `dircolors ~/.dircolors-light`
 
-# nvm
-NVM_DIR=~/.nvm
-[ -s "/home/srijan/.nvm/nvm.sh" ] && . "/home/srijan/.nvm/nvm.sh"
-
 # rvm
 [[ -s '~/.rvm/scripts/rvm' ]] && . '~/.rvm/scripts/rvm'
 
