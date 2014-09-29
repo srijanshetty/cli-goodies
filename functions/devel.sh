c6744286f8b0adaba1ecf4fb568bc243ed29ddb3
Srijan R Shetty <srijan.shetty@gmail.com>
2014-09-29 19:18:18 +0530

    PATH for different languages

--- functions/devel.sh
+++ functions/devel.sh
@@ -9,3 +9,9 @@ function pastebin() {
 alias chromium="/home/srijan/Documents/local/chrome-linux/chrome"
 export CHROME_DEVEL_SANDBOX="/home/srijan/Documents/local/chrome-linux/chrome_sandbox"
 
+# Go
+export GOPATH=$HOME/.go
+export PATH=$PATH:$GOPATH/bin
+
+# Cabal
+export PATH=$PATH:~/.cabal/bin
