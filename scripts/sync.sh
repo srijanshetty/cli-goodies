d4207ae45b8da16950d07773076b8713cd90cc6a
Srijan R Shetty <srijan.shetty@gmail.com>
2014-12-24 18:54:41 +0530

    using rm instead of trash for better interoperability

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -5,8 +5,8 @@ setopt dotglob # To include hidden files
 files=(./$1/*)
 
 if [ ${#files[@]} -gt 0 ]; then
-    rsync $1/ $2/ -a --copy-links -v
+    rsync "$1/" "$2/" -a --copy-links -v
     for file in "${files[@]}"; do
-        trash $file
+        rm -f $file
     done
 fi
