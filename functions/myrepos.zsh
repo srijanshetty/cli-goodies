c3bb91bb4a2c3062804724a370992386b20c8f63
Srijan R Shetty <srijan.shetty@gmail.com>
2015-02-05 01:03:38 +0530

    completions for repos

--- functions/myrepos.zsh
+++ functions/myrepos.zsh
@@ -10,14 +10,14 @@ USAGE: repos <command> [arguments]
 
 COMMAND:
 
-    add-git         add a github configuration
-    add-bit         add a bitbucket configuration
-    available       list available configurations
-    check           list all repos not in mrconfig
-    disable         diable a configuration
-    enable          enable a configuration
-    list            list current configurations
-    only            only enable one configuration
+    --add-git         add a github configuration
+    --add-bit         add a bitbucket configuration
+    --available       list available configurations
+    --check           list all repos not in mrconfig
+    --disable         diable a configuration
+    --enable          enable a configuration
+    --list            list current configurations
+    --only            only enable one configuration
 
 _EOH_
     }
@@ -61,14 +61,14 @@ _EOH_
     }
 
     case "$1" in
-        ls | list)           _repos-list;;
-        e | enable)         _repos-enable "$2";;
-        d | disable)        _repos-disable "$2";;
-        a | avaliable)      _repos-available;;
-        c | check)          _repos-check;;
-        ag | add-git)       _repos-add-git "$2";;
-        ab | add-bit)       _repos-add-bit "$2";;
-        o | only)           _repos-only "$2";;
+        -ls | --list)           _repos-list;;
+        -e | --enable)         _repos-enable "$2";;
+        -d | --disable)        _repos-disable "$2";;
+        -a | --avaliable)      _repos-available;;
+        -c | --check)          _repos-check;;
+        -ag | --add-git)       _repos-add-git "$2";;
+        -ab | --add-bit)       _repos-add-bit "$2";;
+        -o | --only)           _repos-only "$2";;
         *)                  _repos-help;;
     esac
 
