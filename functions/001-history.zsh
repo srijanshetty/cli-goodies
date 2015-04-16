d2a34c5928c92cf343f72c87f9bf97e0d3f8cf59
Srijan R Shetty <srijan.shetty@gmail.com>
2015-04-16 12:27:07 +0530

    Record all install messages to install-log

--- functions/001-history.zsh
+++ functions/001-history.zsh
@@ -37,3 +37,17 @@ setopt HIST_BEEP                 # Beep when accessing non-existent history.
 
 # Lists the ten most used commands.
 alias history-stat="history 0 | awk '{print \$2}' | sort | uniq -c | sort -n -r | head"
+
+# Create the log if it does not exist
+[ ! -e "$HOME/.install-log" ] && touch "$HOME/.install-log"
+
+# Store an install log of all npm/pip/apt-get installs
+preexec() {
+    if [ $1 =~ "install" ]; then
+        echo "$1" >> "${HOME}/.install-log"
+    fi
+
+    if [ $1 =~ "add-apt-repository" ]; then
+        echo "$1" >> "${HOME}/.install-log"
+    fi
+}
