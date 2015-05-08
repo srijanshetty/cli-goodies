213b1480f8789d2f46babe0cc705a9b200a4481b
Srijan R Shetty <srijan.shetty@gmail.com>
2015-05-08 10:57:27 +0530

    Move git and tmux into devel

--- functions/999-devel.zsh
+++ functions/999-devel.zsh
@@ -20,6 +20,133 @@
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #
+# tmux
+#
+# Startup configuration for tmux
+function startt() {
+    case $1 in
+        "iitk")
+            # Create a new tmux-session
+            SESSION="firewall"
+            tmux -2 new-session -d -s $SESSION
+
+            # Create the windows
+            tmux rename-window -t $SESSION:1 firewall
+
+            # Start defualt programs in them
+            tmux send-keys -t $SESSION:1 "auth" C-m
+
+            # Create another tmux-sesion for working
+            SESSION=$(basename $PWD)
+            tmux -2 new-session -d -s $SESSION
+
+            # General purpose
+            tmux rename-window -t $SESSION:1 general-purpose
+            tmux split-window -h
+
+            # The vim window
+            tmux new-window -t $SESSION -a -n vim
+            tmux split-window -h
+            tmux select-pane -t 1
+            tmux resize-pane -R 30
+
+            # An empty window
+            tmux new-window -t $SESSION -a
+
+            # Attach the session
+            tmux select-window -t $SESSION:3
+            tmux attach -t $SESSION
+            ;;
+
+        "project")
+            # Create another tmux-sesion for working
+            SESSION=$(basename $PWD)
+            tmux -2 new-session -d -s $SESSION
+
+            # General purpose
+            tmux rename-window -t $SESSION:1 general-purpose
+            tmux split-window -h
+
+            # The vim window
+            tmux new-window -t $SESSION -a -n vim
+            tmux split-window -h
+            tmux select-pane -t 1
+            tmux resize-pane -R 30
+
+            # An empty window
+            tmux new-window -t $SESSION -a
+
+            # Attach the session
+            tmux select-window -t $SESSION:3
+            tmux attach -t $SESSION
+            ;;
+
+        *)
+            # Create another tmux-sesion for working
+            SESSION="General"
+            tmux -2 new-session -d -s $SESSION
+
+            # General purpose
+            tmux rename-window -t $SESSION:1 general-purpose
+            tmux send-keys -t $SESSION:1 "autostart" C-m
+            tmux split-window -h
+            tmux select-pane -t 1
+            tmux resize-pane -R 30
+
+            # The vim window
+            tmux new-window -t $SESSION -a -n vim
+
+            # An empty window
+            tmux new-window -t $SESSION -a -n irc 'irssi'
+
+            # Attach the session
+            tmux select-window -t $SESSION:3
+            tmux attach -t $SESSION
+            ;;
+
+    esac
+}
+
+#
+# git
+#
+#Git alias
+alias glg="git lg"
+alias gs="git status"
+alias ga="git add"
+alias gt="git tag"
+alias gc="git checkout"
+alias gb="git branch"
+alias gd="git diff"
+alias gp="git push"
+alias gst="git stash"
+alias gcm="git commit -m"
+alias gca="git commit --amend"
+alias gr="git remote"
+alias grv="git remote -v"
+
+# If you give the command cd, then it goes to the git root if in a git directory
+# or goes to $HOME of the user if not in a git directory
+_git_cd () {
+    if [[ "$1" != "" ]]; then
+        cd "$@"
+    else
+        local OUTPUT
+        OUTPUT="$(git rev-parse --show-toplevel 2>/dev/null)"
+        if [[ -e "$OUTPUT" ]]; then
+            if [[ "$OUTPUT" != "$(pwd)" ]]; then
+                cd "$OUTPUT"
+            else
+                cd
+            fi
+        else
+            cd
+        fi
+    fi
+}
+alias cd=_git_cd
+
+#
 # Go
 #
 export GOPATH=$HOME/.go
