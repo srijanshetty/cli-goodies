9c214a429e81f4233699865636af099dc57c1cee
Srijan R Shetty <srijan.shetty@gmail.com>
2015-05-14 11:31:07 +0530

    Convert some functions to autoloadable functions

--- settings/100-functions.zsh
+++ settings/100-functions.zsh
@@ -40,48 +40,11 @@ function md() {
     mkdir "$1" && cd "$1"
 }
 
-# Set and unset proxy
-function proxy() {
-    case $1 in
-        u|unset)
-            unset {http,https,ftp,socks,rvm}_proxy
-            ;;
-        s|status)
-            env | grep proxy
-            ;;
-        *)
-            source ~/.proxyrc
-            ;;
-    esac
-}
-
-# Taken for @addyosmani's dotfiles
-# Start an HTTP server from a directory, optionally specifying the port
-function server() {
-    local port="${1:-8000}"
-    o "http://localhost:${port}/"
-    # Set the default Content-Type to `text/plain` instead of `application/octet-stream`
-    # And serve everything as UTF-8 (although not technically correct, this doesn’t break anything for binary files)
-    python -c $'import SimpleHTTPServer;\nmap = SimpleHTTPServer.SimpleHTTPRequestHandler.extensions_map;\nmap[""] = "text/plain";\nfor key, value in map.items():\n\tmap[key] = value + ";charset=UTF-8";\nSimpleHTTPServer.test();' "$port"
-
-    # python -m SimpleHTTPServer "$port"
-}
-
 ######################################################################
 
 # To get the value of error codes
 function errorcode() {
     python -c "import os; print os.strerror($1)";
-}
-
-# send a directory
-function sdp() {
-    tar cvzf - "$1" --exclude-vcs | ssh "$2" 'tar xzf -'
-}
-
-#pastebin
-function pastebin() {
-    curl -F 'sprunge=<-' http://sprunge.us < "${1:-/dev/stdin}";
 }
 
 # https://transfer.sh/
