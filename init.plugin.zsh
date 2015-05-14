54da0feb346b546075ffe93edb617eadf60a0a52
Srijan R Shetty <srijan.shetty@gmail.com>
2015-05-14 11:30:28 +0530

    Autoload autoloadable functions in functions

--- init.plugin.zsh
+++ init.plugin.zsh
@@ -19,10 +19,21 @@
 # IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+#
+# Settings
+#
 # Source files in functions directory
 for file in ${0:h}/settings/**/*(.); do
     source "$file"
 done
+
+#
+# Scripts
+#
+
+# Add the scripts folder to PATH
+PLUGIN_D="$(dirname $0)"
+export PATH="${PATH}:${PLUGIN_D}/scripts"
 
 # File that have been taken from other git repos
 if [ -e "${0:h}/external" ]; then
@@ -31,12 +42,19 @@ if [ -e "${0:h}/external" ]; then
     done
 fi
 
-# Add the scripts folder to PATH
-PLUGIN_D="$(dirname $0)"
-export PATH="${PATH}:${PLUGIN_D}/scripts"
+
+#
+# Functions and Completions
+#
 
 # Add completions to $fpath.
 fpath=("${0:h}/functions" $fpath)
+
+# Load functions which are not completions
+for pfunction in ${0:h}/functions/^[_.]*; do
+  func=$(echo $pfunction | tr '/' ' ' | awk '{print $NF}')
+  autoload -Uz "$func"
+done
 
 # Load and initialize the completion system ignoring insecure directories.
 autoload -Uz compinit && compinit -i
