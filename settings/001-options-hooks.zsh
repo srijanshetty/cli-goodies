1b9b006ece286e2ce5b575f80a37d6e88b56644b
Srijan R Shetty <srijan.shetty@gmail.com>
2015-05-16 22:06:24 +0530

    Fix prexec error by using [[

--- settings/001-options-hooks.zsh
+++ settings/001-options-hooks.zsh
@@ -12,27 +12,27 @@ setopt AUTO_CD              # Writing the name of directory cd's into it
 
 # Store an install log of all npm/pip/apt-get installs
 preexec() {
-    if [ $1 =~ "^npm install" ]; then
+    if [[ $1 =~ "^npm install" ]]; then
         echo "$1" >> "${HOME}/.install-log"
     fi
 
-    if [ $1 =~ "^gem install" ]; then
+    if [[ $1 =~ "^gem install" ]]; then
         echo "$1" >> "${HOME}/.install-log"
     fi
 
-    if [ $1 =~ "^pip install" ]; then
+    if [[ $1 =~ "^pip install" ]]; then
         echo "$1" >> "${HOME}/.install-log"
     fi
 
-    if [ $1 =~ "^go (get|install)" ]; then
+    if [[ $1 =~ "^go (get|install)" ]]; then
         echo "$1" >> "${HOME}/.install-log"
     fi
 
-    if [ $1 =~ "add-apt-repository" ]; then
+    if [[ $1 =~ "add-apt-repository" ]]; then
         echo "$1" >> "${HOME}/.install-log"
     fi
 
-    if [ $1 =~ "apt-get install" ]; then
+    if [[ $1 =~ "apt-get install" ]]; then
         echo "$1" >> "${HOME}/.install-log"
     fi
 }
