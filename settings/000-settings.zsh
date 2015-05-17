6bd8f48f4ea7df3a4f45e44b25a58649d451e425
Srijan R Shetty <srijan.shetty@gmail.com>
2015-05-17 11:00:12 +0530

    Merge all options into 1

--- settings/000-settings.zsh
+++ settings/000-settings.zsh
@@ -1,4 +1,23 @@
 #
+# Source
+#
+
+# source autojump configuration
+[[ -s ~/.autojump/etc/profile.d/autojump.zsh ]] && . ~/.autojump/etc/profile.d/autojump.zsh
+
+# source proxy configuration
+# . ~/.proxyrc
+
+# source private functions
+[[ -s ~/.private.sh ]] && source ~/.private.sh
+
+# mr
+[[ -s ~/.mrenv ]] && source ~/.mrenv
+
+# fzf
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+#
 # Directory options
 #
 setopt AUTO_CD              # Writing the name of directory cd's into it
