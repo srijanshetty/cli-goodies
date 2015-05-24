3c78d90c9fe33aa6643b5e9b6c2fca791af84363
Srijan R Shetty <srijan.shetty@gmail.com>
2015-05-24 15:25:55 +0530

    Remove scripts folder

--- init.plugin.zsh
+++ init.plugin.zsh
@@ -5,6 +5,13 @@
 #
 source "${0:h}/init.zsh"
 
+#
+# Functions and Completions
+#
+
+# Add completions to $fpath.
+fpath=("${0:h}/functions" $fpath)
+
 # Load functions which are not completions
 for pfunction in ${0:h}/functions/^[_.]*; do
   func=$(echo $pfunction | tr '/' ' ' | awk '{print $NF}')
--- init.zsh
+++ init.zsh
@@ -7,18 +7,3 @@
 for file in ${0:h}/settings/**/*(.); do
     source "$file"
 done
-
-#
-# Scripts
-#
-
-# Add the scripts folder to PATH
-PLUGIN_D="$(dirname $0)"
-export PATH="${PATH}:${PLUGIN_D}/scripts"
-
-#
-# Functions and Completions
-#
-
-# Add completions to $fpath.
-fpath=("${0:h}/functions" $fpath)
