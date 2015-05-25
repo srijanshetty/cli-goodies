58e9a6f2aaf95f0cbb79a4c34face2b8c71710c0
Srijan R Shetty <srijan.shetty@gmail.com>
2015-05-25 15:12:31 +0530

    move git aliases to dwim

--- settings/999-devel.zsh
+++ settings/999-devel.zsh
@@ -9,24 +9,6 @@ _dwim_add_transform '^cp ' \
 _dwim_add_transform '^mv ' \
   '_dwim_sed "s/^mv.*/cd \!\$/"'
 
-# gd -> ga
-_dwim_add_transform '^gd ' \
-  '_dwim_sed "s/^gd /ga /"'
-
-## git clone -> git clone --recursive
-_dwim_prepend_transform '^git clone' \
-  '_dwim_sed "s/git clone /git clone --recursive /"'
-
-## git rm -> git rm -f (on failure)
-_dwim_prepend_transform '^git rm' \
-  '_dwim_sed "s/git rm /git rm -f /"' \
-  1
-
-## git rm -f -> git rm -rf (on failure)
-_dwim_prepend_transform '^git rm -f' \
-  '_dwim_sed "s/git rm -f /git rm -rf /"' \
-  1
-
 #
 # git
 #
