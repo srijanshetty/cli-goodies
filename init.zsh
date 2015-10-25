99ca387dbf42dec4f8127b8846ccf24aaecaf1dc
Srijan R Shetty <srijan.shetty@gmail.com>
2015-10-25 11:11:43 +0000

    Create a simple plugin system

--- init.zsh
+++ init.zsh
@@ -7,3 +7,12 @@
 for file in ${0:h}/settings/**/*(.); do
     source "$file"
 done
+
+#
+# Plugins
+#
+
+# Load functions which are not completions
+for pfunction in ${0:h}/plugins/**/*.plugin.zsh; do
+    source $pfunction
+done
