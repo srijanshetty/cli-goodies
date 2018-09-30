5faaf4370454787dab91c85f5c36eb44173683bb
Srijan R Shetty <srijan.shetty@gmail.com>
2018-09-30 19:22:58 +0530

    default alias for tmux, top, utilities for sha & external ip

--- settings/000-settings.zsh
+++ settings/000-settings.zsh
@@ -59,6 +59,18 @@ alias vs="vcsh status"
 # List all global npm packages
 alias npm-list="npm list -g --depth 0"
 
+# unicode support in tmux
+alias tmux="tmux -u"
+
+# top, the way it should be
+alias top="top -c"
+
+# Quick check for sha
+alias sha='shasum -a 256 '
+
+# ips
+alias ipe='curl ipinfo.io/ip'
+
 #
 # Hooks
 #
