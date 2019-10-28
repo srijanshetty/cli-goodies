eb35efce79676eee066d1072900e3b8b8c5a2511
Srijan R Shetty <srijan.shetty@gmail.com>
2019-10-28 15:44:01 +0530

    feat: update alias used in git

--- settings/100-git.zsh
+++ settings/100-git.zsh
@@ -3,17 +3,15 @@
 ###########################################
 alias glg="git lg"
 alias gs="git status"
-alias ga="git add --all"
 alias gt="git tag"
 alias gc="git checkout"
 alias gb="git branch"
 alias gd="git diff"
+alias gdc="git diff --cached"
 alias gst="git stash"
 alias gcm="git commit -m"
 alias gca="git commit --amend"
-alias gr="git remote"
 alias grv="git remote -v"
-alias gig="git ignore"
 
 # If you give the command cd, then it goes to the git root if in a git directory
 # or goes to $HOME of the user if not in a git directory
