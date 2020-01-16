f9677bf82277a445f9d9207c4441aa6134b5b8df
Srijan R Shetty <srijan.shetty@gmail.com>
2020-01-16 13:38:56 +0530

    feat: experimental count function

--- settings/999-experimental.zsh
+++ settings/999-experimental.zsh
@@ -5,3 +5,6 @@ alias now='date -u +"%Y-%m-%dT%H:%M:%SZ"' # print current datetime as ISO
 function copy-last-command() {
   fc -ln -1 | awk "{\$1=\$1}1" ORS="" | pbcopy
 }
+
+# count occurences
+alias count='sort | uniq -c | sort -n'
