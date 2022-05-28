ac05a89c2eba62a1375095815273a7efa8b7f8d5
Srijan R Shetty <srijan.shetty@gmail.com>
2022-05-28 16:25:22 +0530

    fix: comments for fzf git commands

--- settings/101-fzf.zsh
+++ settings/101-fzf.zsh
@@ -32,6 +32,7 @@ if [[ -f ~/.fzf.zsh ]]; then
     }
 
     function _gf() {
+        # List git modified files
         is_in_git_repo || return
         git -c color.status=always status --short |
             fzf-down -m --ansi --nth 2..,.. \
@@ -40,6 +41,7 @@ if [[ -f ~/.fzf.zsh ]]; then
         }
 
     function _gb() {
+        # List git branches
         is_in_git_repo || return
         git branch -a --color=always | grep -v '/HEAD\s' | sort |
             fzf-down --ansi --multi --tac --preview-window right:70% \
@@ -49,6 +51,7 @@ if [[ -f ~/.fzf.zsh ]]; then
         }
 
     function _gt() {
+        # List git tags
         is_in_git_repo || return
         git tag --sort -version:refname |
             fzf-down --multi --preview-window right:70% \
@@ -56,6 +59,7 @@ if [[ -f ~/.fzf.zsh ]]; then
         }
 
     function _gr() {
+        # List git remotes
         is_in_git_repo || return
         git remote -v | awk '{print $1 "\t" $2}' | uniq |
             fzf-down --tac \
@@ -64,6 +68,7 @@ if [[ -f ~/.fzf.zsh ]]; then
         }
 
     function _gk() {
+        # List git commits
         is_in_git_repo || return
         git log --date=short --format="%C(green)%C(bold)%cd %C(auto)%h%d %s (%an)" --graph --color=always |
             fzf-down --ansi --no-sort --reverse --multi --bind 'ctrl-s:toggle-sort' \
